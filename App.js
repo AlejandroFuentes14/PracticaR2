fbad9ec2f1571d00c70c44fcf711c5fc59fe406e
AlejandroFuentes14 <jfuentes0@ucol.mx>
2023-11-09 13:56:13 -0600

    Practica R2 LoginView lista

--- App.js
+++ App.js
@@ -1,54 +1,93 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Switch, TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, Image } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      switchValue: false,
-      textValue: '',
+      email: '',
+      password: '',
     };
   }
-  onChange = (value) => {
-    console.warn(`El switch cambiará a: ${value}`)
-    this.setState({switchValue: value})
+
+  validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   }
-  onChangeText = (text) => {
-    this.setState({textValue: text})
+
+  validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    return regex.test(password);
   }
-  onPressLearnMore = () => {
-    console.warn("Presionaste el botón")
-    Alert.alert("Texto ingresado", this.state.textValue)
+
+  onPressLogin = () => {
+    const { email, password } = this.state;
+    if (!this.validateEmail(email)) {
+      Alert.alert("Error", "Email no válido");
+      return;
+    }
+    if (!this.validatePassword(password)) {
+      Alert.alert("Error", "La contraseña debe tener al menos 8 caracteres, mayúsculas, minúsculas y al menos 1 caracter especial");
+      return;
+    }
+    Alert.alert("Éxito", "Inicio de sesión exitoso");
   }
-  render(){
+
+  render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={this.onChangeText}
-          value={this.state.textValue}
-        />
-        <Switch 
-          onValueChange={this.onChange}
-          value = {this.state.switchValue}/>
-        <Button
-          onPress={this.onPressLearnMore}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Text style={styles.welcome}>MyGames -- Fuentes</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Image source={{uri: 'https://portal.ucol.mx/content/micrositios/158/image/UdeC%20Abajo_Negro.png'}} style={styles.image} />
+        <View style={styles.formContainer}>
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({email: text})}
+            value={this.state.email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Text style={styles.label}>Contraseña:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({password: text})}
+            value={this.state.password}
+            secureTextEntry={true}
+          />
+          <Button
+            onPress={this.onPressLogin}
+            title="Iniciar sesión"
+            color="#841584"
+          />
+        </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 16,
+    backgroundColor: '#F5FCFF',
+  },
+  formContainer: {
+    marginTop: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
   },
 });
